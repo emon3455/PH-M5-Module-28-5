693d0f31dceae57b1ccb4cc6239743bd7783e61c
emon3455 <emon.mhk69@gmail.com>
2023-02-14 01:39:33 +0600

    to do list done

--- js/index.js
+++ js/index.js
@@ -12,24 +12,56 @@ document.getElementById("add-btn").addEventListener("click", function(){
     <td class="counters"> ${counter} </td>
     <td class="values"> ${values} </td>
     <td>
-    <button class="btn btn-warning mb-1">Done</button>
+    <button class="btn btn-warning mb-1 dones">Done</button>
     <button class="btn btn-danger drops">Drop</button>
     </td>
     `;
 
     parent.appendChild(tr);
 
+    // const drops = document.getElementsByClassName("drops");
+    // for (const drop of drops) {
+    // drop.addEventListener("click" , function(event){
+    //     event.target.parentNode.parentNode.style.display = "none";
+
+    // });
+
+});
+
+// drop event
+document.getElementById("add-btn").addEventListener("click", function(){
+
+    const drops = document.getElementsByClassName("drops");
+
+    for (const drop of drops) {
+    drop.addEventListener("click" , function(event){
+        event.target.parentNode.parentNode.style.display = "none";
+        
+    });
+
+}
+
+});
+
+// drop event
+document.getElementById("add-btn").addEventListener("click", function(){
+
+    const dones = document.getElementsByClassName("dones");
+    for (const done of dones) {
+        done.addEventListener("click" , function(event){
+            event.target.parentNode.parentNode.style.backgroundColor="pink";
+        });
+    }
+
+
 });
 
 
+// remove all event:
 document.getElementById("clear-btn").addEventListener("click" , function(){
-
-    
+  
     const parent = document.getElementById("table-body");
-    parent.innerHTML= "";
+    parent.style.display="none";
     counter=0;
 
 });
-
-
-document.getElementById("")
