94052780ae2caa860864fe5b3bcfaac42f32ae98
emon3455 <emon.mhk69@gmail.com>
2023-02-14 00:49:16 +0600

    clear all done

--- js/index.js
+++ js/index.js
@@ -9,18 +9,27 @@ document.getElementById("add-btn").addEventListener("click", function(){
     let tr = document.createElement("tr");
     tr.innerHTML = `
     
-    <td> ${counter} </td>
-    <td> ${values} </td>
+    <td class="counters"> ${counter} </td>
+    <td class="values"> ${values} </td>
     <td>
-    <button class="btn btn-warning">Done</button>
-    <button class="btn btn-danger">Drop</button>
+    <button class="btn btn-warning mb-1">Done</button>
+    <button class="btn btn-danger drops">Drop</button>
     </td>
     `;
 
     parent.appendChild(tr);
 
+});
 
 
+document.getElementById("clear-btn").addEventListener("click" , function(){
 
+    
+    const parent = document.getElementById("table-body");
+    parent.innerHTML= "";
+    counter=0;
 
 });
+
+
+document.getElementById("")
